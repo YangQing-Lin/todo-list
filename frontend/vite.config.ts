275eba5456752c74410b8a6a711ea0e50897b92f
Luke Lin <luke.lin@wintoo.com>
2025-11-19 15:36:06 +0800

    解决其他地方调用服务异常的问题

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
         port: 3000,
         proxy: {
             '/api': {
-                target: 'http://localhost:7789',
+                target: 'http://127.0.0.1:7789',
                 changeOrigin: true,
             }
         }
